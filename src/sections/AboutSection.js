0226bdfec7dafd84c0cd53f860b70a7e6d7ebe5a
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2024-04-29 07:53:57 +0100

    ui update

--- src/sections/AboutSection.js
+++ src/sections/AboutSection.js
@@ -3,16 +3,16 @@ import About from "../images/about.png"
 
 function AboutSection() {
     return (
-        <section className="max-w-8xl mx-auto w-full bg-transparent h-[90vh] flex flex-col justify-center items-center py-8 mt-20" id="about">
-            <div className="flex flex-row justify-center items-start gap-10 px-32 py-2">
-                <span className="flex flex-col gap-4 text-white w-[45%]">
+        <section className="max-w-8xl mx-auto w-full bg-transparent h-[fit-content] lg:h-[90vh] flex flex-col justify-center items-center py-8 mt-5 lg:mt-20" id="about">
+            <div className="flex flex-col lg:flex-row justify-center items-center lg:items-start gap-10 px-6 py-2 lg:px-32 lg:py-2">
+                <span className="flex flex-col gap-4 text-white w-full lg:w-[45%]">
                     <h1 className="text-[1rem] font-bold">About the Luxury Queen Pageant</h1>
-                    <h2 className="text-[1.5rem] max-w-[85%] font-semibold">We are the most prestigious beauty pageant in the whole of Nigeria
+                    <h2 className="text-[1.5rem] lg:max-w-[85%] font-semibold">We are the most prestigious beauty pageant in the whole of Nigeria
                         and we give you a stage to achieve your pageantry dreams. 
                     </h2>
                     <button className="bg-black border-white border w-[50%] text-white px-4 py-2 rounded-lg mt-4">Register Now</button>
                 </span>
-                <div className="w-[50%] h-[500px]">
+                <div className=" w-[85%] h-[360px] lg:w-[50%] lg:h-[500px]">
                     <img src={About} alt="What we do" className="w-full h-full object-cover" />
                 </div>
             </div>
--- src/sections/HeroSection.js
+++ src/sections/HeroSection.js
@@ -3,9 +3,9 @@ import React from "react";
 function HeroSection() {
     return (
         <section className="relative w-full h-[90vh] text-white bg-hero bg-no-repeat bg-cover  bg-blend-darken flex flex-col justify-center items-center">
-            <div className="absolute flex flex-col justify-center items-center w-full h-[90vh] bg-black bg-opacity-80 text-white">
-                <h1 className="text-6xl font-bold">The Luxury Queen</h1>
-                <p className="text-xl">The most prestigious beauty pageant in the world</p>
+            <div className="absolute flex flex-col justify-center items-center w-full h-full bg-black bg-opacity-80 text-white">
+                <h1 className="text-[2.2rem] lg:text-6xl font-bold">The Luxury Queen</h1>
+                <p className="text-[1rem] text-center lg:text-xl">The most prestigious beauty pageant in the world</p>
                 <button className="bg-white text-black px-4 py-2 rounded-lg mt-4">Register Now</button>
             </div>
         </section>
